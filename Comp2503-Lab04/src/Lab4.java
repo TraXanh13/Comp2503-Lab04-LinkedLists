490679476388ab75c9466b4c8c6a93ef8b0c1ef9
Kyle Duong <kyleduong13@gmail.com>
2019-10-06 14:38:08 -0600

    Why isn't the addInMiddle not working?

--- Comp2503-Lab04/src/Lab4.java
+++ Comp2503-Lab04/src/Lab4.java
@@ -8,7 +8,7 @@ public class Lab4 {
 			head = n;
 			tail = n;
 		} else {
-			head.setNext(n);
+			n.setNext(head);
 			head = n;
 		}
 	}
@@ -22,32 +22,30 @@ public class Lab4 {
 			tail = n;
 		}
 	}
-
+	
 	public void addInOrder(Node<String> n) {
 		Node<String> currentNode = head;
-		boolean isAdded = false;
-		if (head == null) {
+		System.out.println("Adding: " + n.getData());
+		if(head == null) {
 			head = n;
 			tail = n;
-		} else {
-			if (head.getData().compareTo(n.getData()) > 0) {
-				addHead(n);
-			} else {
-				while (currentNode.getNext() != null) {
-					if (currentNode.getNext().getData().compareTo(n.getData()) > 0) {
-						n.setNext(currentNode.getNext());
-						currentNode = n;
-						isAdded = true;
-						break;
-					}else {
-						currentNode = currentNode.getNext();
-					}
+		}else if(head.getData().compareTo(n.getData()) > 0) {
+			addHead(n);
+		}else {
+			while (currentNode.getNext() != null) {
+				System.out.println("in the while");
+				if (currentNode.getNext().getData().compareTo(n.getData()) > 0) {
+					addInMiddle(currentNode, n);
+					break;
 				}
-				if(!isAdded) {
-					addTail(n);
-				}
+				currentNode = currentNode.getNext();
 			}
 		}
+	}
+
+	private void addInMiddle(Node<String> curr, Node<String> n) {
+		n.setNext(curr);
+		curr = n;
 	}
 
 	public Node<String> find(String key) {
@@ -63,13 +61,28 @@ public class Lab4 {
 	}
 
 	public Node<String> delete(String key) {
-		// implement delete
+		Node<String> currentNode = head;
+		if (head.getData().equals(key)) {
+			head = head.getNext();
+		} else {
+			while (currentNode.getNext() != null) {
+				if (currentNode.getNext().getData().equals(key)) {
+					currentNode.setNext(currentNode.getNext().getNext());
+					break;
+				} else {
+					currentNode = currentNode.getNext();
+				}
+			}
+		}
 		return null;
 	}
 
 	public void printList() {
 		System.out.println();
 		Node<String> currentNode = head;
+		if (head == null) {
+			System.out.println("The list is empty");
+		}
 		while (currentNode != null) {
 			// Visit the node. In this case, print it out.
 			System.out.println(currentNode.toString());
@@ -78,19 +91,32 @@ public class Lab4 {
 	}
 
 	public void emptyList() {
-
+		head = null;
+		tail = null;
 	}
 
 	public void run() {
 		Node<String> a = new Node<String>("Athabasca");
-		Node<String> c = new Node<String>("Andromeda");
-		Node<String> b = new Node<String>("Hector");
+		Node<String> b = new Node<String>("Andromeda");
+		Node<String> c = new Node<String>("Hector");
 		Node<String> d = new Node<String>("Victoria");
+		System.out.println("Add everyone to the list");
+		addHead(b);
+		addTail(c);
+		addTail(a);
+		addHead(d);
+		printList();
+		System.out.println("Remove Hector");
+		delete("Hector");
+		printList();
+		emptyList();
+		printList();
 		addInOrder(a);
 		addInOrder(b);
 		addInOrder(c);
 		addInOrder(d);
 		printList();
+
 	}
 
 	public static void main(String[] args) {
